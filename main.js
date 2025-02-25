1b30e20468527c1427267b7cb6f5845665c698e4
이도윤 <dlwhdgus482@naver.com>
2025-02-25 22:05:04 +0900

    api 주소 수정

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const API_KEY = "4d52698a98e84e07a18106b9123fabb0";
 let news = [];
 const getLatestNews = async() => {
     const url = new URL(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        // `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&apiKey=${API_KEY}`
     );
     const response = await fetch(url);
     const data = await response.json();
